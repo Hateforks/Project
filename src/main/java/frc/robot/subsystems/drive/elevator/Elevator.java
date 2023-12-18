0b740b5d45bd6873b4e7b484d66c5abf2702730c
shigureui2 <152223801+shigureui2@users.noreply.github.com>
2023-12-18 21:07:59 +0000

    robot container and elevator changes

--- src/main/java/frc/robot/subsystems/drive/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/drive/elevator/Elevator.java
@@ -1,1 +1,28 @@
+package frc.robot.subsystems.drive.elevator;
 
+import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.networktables.LoggedDashboardNumber;
+
+import edu.wpi.first.wpilibj.RobotBase;
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d;
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.util.Color8Bit;
+import edu.wpi.first.wpilibj.util.Color;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.RobotContainer;
+
+import static frc.robot.Constants.Elevator.*;
+import static frc.robot.Constants.Elevator.ElevatorPhysicalConstants.ELEVATOR_PID;
+
+public class Elevator extends SubsystemBase {
+    private final ElevatorIOInputsAutoLogged inputs = new ElevatorIOInputsAutoLogged();
+
+    private LoggedDashboardNumber p = new LoggedDashboardNumber("Elevator/P", ELEVATOR_PID[0]);
+    private LoggedDashboardNumber i = new LoggedDashboardNumber("Elevator/I", ELEVATOR_PID[1]);
+    private LoggedDashboardNumber d = new LoggedDashboardNumber("Elevator/D", ELEVATOR_PID[2]);
+    private LoggedDashboardNumber ff = new LoggedDashboardNumber("Elevator/FF", ELEVATOR_PID[3]);
+}
